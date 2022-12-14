e63f19fe77cfea0cc0dbf361fe5b84c2c0db0c2e
Edgaras Benediktavicius <725324+edgarasben@users.noreply.github.com>
2022-12-14 22:01:17 +0100

    Init
    - Install NextJS
    - Add /app directory
    - Add favicon
    - Add TailwindCSS
    - Remove unnecessary assets

--- next.config.js
+++ next.config.js
@@ -2,6 +2,8 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
-
-module.exports = nextConfig
+  experimental: {
+    appDir: true,
+    // runtime: 'experimental-edge',
+  },
+};
