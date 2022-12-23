ac31212c4196a88905a702e1eb964eeddf535496
Edgaras Benediktavicius <725324+edgarasben@users.noreply.github.com>
2022-12-23 21:33:30 +0200

    Fix posts revalidation

--- app/posts/page.tsx
+++ app/posts/page.tsx
@@ -2,6 +2,8 @@ import { getPosts } from '@/lib/get-posts'
 import { Posts } from '@/components/posts'
 import { Container } from '@/components/container'
 
+export const revalidate = 30 // revalidate every 30 secs
+
 export default async function PostsPage() {
     const posts = await getPosts()
     return (
