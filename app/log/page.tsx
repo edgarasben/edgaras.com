e7015504291cda26f0244a90a06d95586ec2bc7e
Edgaras Benediktavicius <725324+edgarasben@users.noreply.github.com>
2023-01-22 09:28:38 +0100

    Adjust log colors

--- app/log/page.tsx
+++ app/log/page.tsx
@@ -39,16 +39,16 @@ export default async function PostsPage() {
     return (
         <Container>
             <section>
-                <h2 className="py-8 px-4 text-xl font-semibold">Log</h2>
+                <h2 className="py-8 text-xl font-semibold">Log</h2>
                 <div className="space-y-8">
                     {groupedItems.map((week) => {
                         return (
-                            <div key={week.week} className="space-y-2 px-4">
+                            <div key={week.week} className="space-y-2">
                                 <h2 className="text-sm uppercase tracking-wider text-fg-neutral-faded">
                                     2022 Week {week.week}
                                 </h2>
 
-                                <ul className="overflow-hidden rounded-md shadow-sm">
+                                <ul className="divide-y divide-border-neutral-faded/50 overflow-hidden rounded-md shadow-sm">
                                     {week.logs.map((logItem: any) =>
                                         logItem.url ? (
                                             <li key={logItem.title + ' ' + logItem.url}>
@@ -75,9 +75,9 @@ export default async function PostsPage() {
 function Row({ item }: { item: Log }) {
     return item.content ? (
         <details className="group flex items-center space-x-2 bg-bg-elevated py-2 pl-2 pr-3 open:bg-bg-neutral hover:bg-bg-neutral">
-            <summary className="flex cursor-pointer items-center justify-between">
+            <summary className="flex cursor-pointer items-start justify-between sm:items-center">
                 <RowInner item={item} />
-                <ChevronDownIcon className="h-5 w-5 text-fg-neutral-faded" />
+                <ChevronDownIcon className="h-5 w-5 flex-shrink-0 text-fg-neutral-faded" />
             </summary>
             <div className="mt-3 py-4 text-fg-neutral-faded">{item.content}</div>
         </details>
@@ -96,28 +96,32 @@ function Row({ item }: { item: Log }) {
 
 function RowInner({ item }: { item: Log }) {
     return (
-        <div className="flex items-center space-x-2">
-            {item.tag && (
+        <div className="flex items-start space-x-2 sm:items-center">
+            <div>
+                {item.tag && (
+                    <span
+                        className={`y-0.5 mr-1.5 inline-flex rounded px-2 text-black/70 dark:text-white/70 ${
+                            item.tag === 'Work' ? 'bg-accent-1/50' : ''
+                        }${item.tag === 'Learn' ? 'bg-accent-2/50' : ''}${
+                            item.tag === 'Discover' ? 'bg-accent-3/50' : ''
+                        }`}
+                    >
+                        {item.tag}
+                    </span>
+                )}
                 <span
-                    className={`y-0.5 rounded px-2 text-black/70 dark:text-white/70 ${
-                        item.tag === 'Work' ? 'bg-accent-1' : ''
-                    }${item.tag === 'Learn' ? 'bg-accent-2' : ''}${
-                        item.tag === 'Discover' ? 'bg-accent-3' : ''
-                    }`}
+                    className={
+                        item.url
+                            ? 'decoration-fg-neutral/50 underline-offset-4 group-hover:underline'
+                            : ''
+                    }
                 >
-                    {item.tag}
+                    {item.title}
                 </span>
+            </div>
+            {item.url && (
+                <LinkIcon className="h-4 w-4 flex-shrink-0 text-fg-neutral-faded" />
             )}
-            <span
-                className={
-                    item.url
-                        ? 'decoration-fg-neutral/50 underline-offset-4 group-hover:underline'
-                        : ''
-                }
-            >
-                {item.title}
-            </span>
-            {item.url && <LinkIcon className="h-4 w-4 text-fg-neutral-faded" />}
         </div>
     )
 }
