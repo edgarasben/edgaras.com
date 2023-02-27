05190f285fb06bf0beebc302ad77cd178928775d
Edgaras Benediktavicius <725324+edgarasben@users.noreply.github.com>
2023-02-27 01:56:37 +0100

    Update revalidate.ts

--- pages/api/revalidate.ts
+++ pages/api/revalidate.ts
@@ -10,10 +10,11 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     // This should be the actual path not a rewritten path
     // e.g. for "/blog/[slug]" this should be "/blog/post-1"
     await res.revalidate(req.query.path as string)
+    console.log(`Revalidated "${req.query.path}"`)
     return res.json({ revalidated: true })
   } catch (err) {
     // If there was an error, Next.js will continue
     // to show the last successfully generated page
-    return res.status(500).send('Error revalidating')
+    return res.status(500).send(`Error revalidating "${req.query.path}"`)
   }
 }
