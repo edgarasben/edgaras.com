ef8d8ea8a31a74f23588adc7d0e8fa99a34b6feb
Edgaras Benediktavicius <725324+edgarasben@users.noreply.github.com>
2023-04-12 21:03:05 +0300

    Update layout.tsx

--- app/portfolio/layout.tsx
+++ app/portfolio/layout.tsx
@@ -14,16 +14,26 @@ export const metadata: Metadata = {
       'Design and development services to help you prototype and launch your web or mobile product.',
     images: [
       {
-        url: 'https://edgaras.com/images/og-portfolio.jpg'
+        url: 'https://edgaras.com/images/og-portfolio.jpg',
+        alt: 'Edgaras — Co-Founder, Designer, Coder',
+        width: 1200,
+        height: 630
       }
     ]
   },
   twitter: {
+    card: 'summary_large_image',
+    creator: '@edgarasben',
+    site: '@edgarasben',
+    title: 'Edgaras — Co-Founder, Designer, Coder',
     description:
       'Design and development services to help you prototype and launch your web or mobile product.',
     images: [
       {
-        url: 'https://edgaras.com/images/og-portfolio.jpg'
+        url: 'https://edgaras.com/images/og-portfolio.jpg',
+        alt: 'Edgaras — Co-Founder, Designer, Coder',
+        width: 1200,
+        height: 630
       }
     ]
   }
