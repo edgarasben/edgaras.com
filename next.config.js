3995e36fe33c00d64468592e638e19dcaf8846f1
Edgaras Benediktavicius <725324+edgarasben@users.noreply.github.com>
2023-05-03 20:03:46 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -15,22 +15,32 @@ const nextConfig = {
     ]
   },
   async rewrites() {
-    return {
-      beforeFiles: [
-        // if the host is `app.acme.com`,
-        // this rewrite will be applied
-        {
-          source: '/:path*',
-          has: [
-            {
-              type: 'host',
-              value: 'serviceintent.com'
-            }
-          ],
-          destination: '/portfolio/:path*'
-        }
-      ]
-    }
+    return (
+      {
+        beforeFiles: [
+          // if the host is `app.acme.com`,
+          // this rewrite will be applied
+          {
+            source: '/:path*',
+            has: [
+              {
+                type: 'host',
+                value: 'serviceintent.com'
+              }
+            ],
+            destination: '/portfolio/:path*'
+          }
+        ]
+      },
+      /*     {
+        source: '/images/:slug*',
+        destination: 'https://files3.ams3.cdn.digitaloceanspaces.com/:slug*'
+      } */
+      {
+        source: '/images/:slug*',
+        destination: 'https://files.edgaras.com/images/:slug*'
+      }
+    )
   }
 }
 
