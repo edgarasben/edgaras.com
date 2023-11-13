0123caf30342c54c4c2c1d3446bb193e47641d88
Edgaras Benediktavicius <725324+edgarasben@users.noreply.github.com>
2023-11-13 16:19:57 +0100

    Fix dialog

--- components/Dialog.tsx
+++ components/Dialog.tsx
@@ -1,18 +1,19 @@
 'use client'
 
-import * as React from 'react'
 import * as DialogPrimitive from '@radix-ui/react-dialog'
+import * as React from 'react'
 
-import { cn } from '@/lib/utils'
 import XMarkIcon from './icons/x-mark'
+import { cn } from '@/lib/utils'
 
 const Dialog = DialogPrimitive.Root
 
 const DialogTrigger = DialogPrimitive.Trigger
 
-const DialogPortal = ({ className, ...props }: DialogPrimitive.DialogPortalProps) => (
-  <DialogPrimitive.Portal className={cn(className)} {...props} />
-)
+const DialogPortal = DialogPrimitive.Portal
+
+const DialogClose = DialogPrimitive.Close
+
 DialogPortal.displayName = DialogPrimitive.Portal.displayName
 
 const DialogOverlay = React.forwardRef<
@@ -99,7 +100,10 @@ DialogDescription.displayName = DialogPrimitive.Description.displayName
 
 export {
   Dialog,
+  DialogPortal,
+  DialogOverlay,
   DialogTrigger,
+  DialogClose,
   DialogContent,
   DialogHeader,
   DialogFooter,
