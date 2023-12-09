9173fb526199b054ef900ce8b73a3af7ae33c123
Edgaras Benediktavicius <725324+edgarasben@users.noreply.github.com>
2023-12-09 17:04:12 +0100

    Update route.ts

--- src/app/api/bookmarks/create/route.ts
+++ src/app/api/bookmarks/create/route.ts
@@ -1,16 +1,20 @@
 import { supabaseAdmin } from '@/lib/supabase/admin'
+import * as cheerio from 'cheerio'
 
 export async function POST(request: Request) {
   try {
     const body = await request.json()
+    const link = body.url
 
     if (body.token !== process.env.API_ACCESS_KEY) {
       return Response.json({ message: 'Invalid access token' })
     }
 
+    const title = await getMetaTitle(link)
+
     const { data, error } = await supabaseAdmin
       .from('bookmarks')
-      .insert({ link: body.url })
+      .insert({ link: link, title: title })
       .select()
       .single()
 
@@ -23,5 +27,17 @@ export async function POST(request: Request) {
     }
   } catch (error) {
     return Response.json({ error: 'Unknown error occured' })
+  }
+}
+
+async function getMetaTitle(url: string) {
+  try {
+    const response = await fetch(url)
+    const body = await response.text()
+    const $ = cheerio.load(body)
+    const title = $('title').text()
+    return title
+  } catch (error) {
+    console.error('Error fetching title:', error)
   }
 }
