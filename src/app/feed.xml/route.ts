2b32a5fd76788829ee3e8af82b17d4cf7a3e120a
Edgaras Benediktavicius <725324+edgarasben@users.noreply.github.com>
2024-01-11 03:27:15 +0100

    refactor: use site config

--- src/app/feed.xml/route.ts
+++ src/app/feed.xml/route.ts
@@ -1,18 +1,20 @@
 import { getArticles } from '@/data/queries'
 import RSS from 'rss'
+import config from '@/site.config.json'
+
+export const revalidate = 0 // 0 seconds
 
 export async function GET() {
   const feed = new RSS({
-    title: 'edgaras.com',
+    title: config.domain,
     description: 'Edgaras is a Software Designer and Developer',
-    generator: 'RSS for Node and Next.js',
-    feed_url: 'https://edgaras.com/feed.xml',
-    site_url: 'https://edgaras.com/',
+    feed_url: `${config.baseUrl}/feed.xml`,
+    site_url: `${config.baseUrl}/`,
     managingEditor: 'hi@edgaras.com (Edgaras Benediktavicius)',
     webMaster: 'hi@edgaras.com (Edgaras Benediktavicius)',
-    copyright: `Copyright ${new Date()
-      .getFullYear()
-      .toString()}, Edgaras Benediktavicius`,
+    copyright: `Copyright ${new Date().getFullYear().toString()}, ${
+      config.firstName
+    } ${config.lastName}`,
     language: 'en-US',
     pubDate: new Date().toUTCString(),
     ttl: 60,
@@ -24,8 +26,8 @@ export async function GET() {
     articles.map((article) => {
       feed.item({
         title: article.title,
-        description: article.description || '',
-        url: `https://edgaras.com/${article.slug}`,
+        description: article.description,
+        url: `${config.baseUrl}/${article.slug}`,
         categories: article.tags?.split(',') || [],
         author: 'Edgaras Benediktavicius',
         date: article.published_at || '',
