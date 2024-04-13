4b04cf51240c266af8a02b5eee9253f2cb15b16e
Edgaras Benediktavicius <725324+edgarasben@users.noreply.github.com>
2024-04-13 17:44:19 +0200

    .

--- next.config.js
+++ next.config.js
@@ -13,12 +13,26 @@ const nextConfig = {
   async rewrites() {
     return [
       {
-        source: '/images/:slug*',
-        destination: 'https://files.edgaras.com/images/:slug*'
+        source: '/images/:path*',
+        destination: 'https://files.edgaras.com/images/:path*'
       },
       {
         source: '/dashboard',
         destination: 'https://arc.net/e/47F91214-C345-42DF-BFDA-8272E3F53567'
+      }
+    ]
+  },
+  async redirects() {
+    return [
+      {
+        source: '/posts/:slug',
+        destination: '/:slug',
+        permanent: true
+      },
+      {
+        source: '/articles/:slug',
+        destination: '/:slug',
+        permanent: true
       }
     ]
   }
