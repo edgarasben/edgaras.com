01021b944fb7fa8ac3a1d97307ae5e725028f44a
Edgaras Benediktavicius <725324+edgarasben@users.noreply.github.com>
2024-04-22 23:06:48 +0200

    .

--- src/app/portfolio/layout.tsx
+++ src/app/portfolio/layout.tsx
@@ -3,16 +3,19 @@ import type { Metadata } from 'next'
 export const metadata: Metadata = {
   title: 'Portfolio',
   openGraph: {
+    title: 'Edgaras — Software Designer and Developer',
     description:
       'Design and development services to help you prototype and launch your web or mobile product.',
+    type: 'website',
+    url: 'https://edgaras.com/portfolio',
     images: [
       {
         url: 'https://edgaras.com/images/og-portfolio.jpg',
         alt: 'Edgaras — Software Designer and Developer',
         width: 1200,
-        height: 630,
-      },
-    ],
+        height: 630
+      }
+    ]
   },
   twitter: {
     card: 'summary_large_image',
@@ -26,14 +29,14 @@ export const metadata: Metadata = {
         url: 'https://edgaras.com/images/og-portfolio.jpg',
         alt: 'Edgaras — Software Designer and Developer',
         width: 1200,
-        height: 630,
-      },
-    ],
-  },
+        height: 630
+      }
+    ]
+  }
 }
 
 export default function PortfolioLayout({
-  children,
+  children
 }: {
   children: React.ReactNode
 }) {
