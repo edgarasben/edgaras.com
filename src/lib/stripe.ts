3064d018d5e0810db14af7d7863eb1e79268ef89
Edgaras Benediktavicius <725324+edgarasben@users.noreply.github.com>
2024-04-26 02:00:22 +0200

    Update stripe.ts

--- src/lib/stripe.ts
+++ src/lib/stripe.ts
@@ -1,11 +1,13 @@
 /**
  * This is a singleton to ensure we only instantiate Stripe once.
  */
-import { Stripe, loadStripe } from '@stripe/stripe-js'
+import { Stripe } from '@stripe/stripe-js'
+import { loadStripe } from '@stripe/stripe-js/pure'
 
 let stripePromise: Promise<Stripe | null>
 const getStripe = () => {
   if (!stripePromise) {
+    loadStripe.setLoadParameters({ advancedFraudSignals: false })
     stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY!)
   }
   return stripePromise
