c45312907af9f5e735132ed7ee8ae9b258f84219
edgarasben <725324+edgarasben@users.noreply.github.com>
2024-10-17 01:11:32 +0700

    .

--- src/app/api/auth/callback/route.ts
+++ src/app/api/auth/callback/route.ts
@@ -1,5 +1,5 @@
-import { NextResponse } from 'next/server'
 import { createServerClient } from '@/lib/supabase/server'
+import { NextResponse } from 'next/server'
 
 export async function GET(request: Request) {
   // The `/api/auth/callback` route is required for the server-side auth flow implemented
@@ -10,7 +10,7 @@ export async function GET(request: Request) {
   const originPath = requestUrl.searchParams.get('originPath')
 
   if (code) {
-    const supabase = createServerClient()
+    const supabase = await createServerClient()
     await supabase.auth.exchangeCodeForSession(code)
   }
 
